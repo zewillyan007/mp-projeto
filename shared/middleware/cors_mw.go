b28a37aaa5b2d739710e038f2532f12b180883d7
Willyan <dev28@redepos.com.br>
2023-04-12 20:33:16 -0300

    cors

--- shared/middleware/cors_mw.go
+++ shared/middleware/cors_mw.go
@@ -7,8 +7,8 @@ import (
 func Cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Access-Token")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST, OPTIONS, DELETE")
+		//w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Access-Token")
+		//w.Header().Set("Access-Control-Allow-Methods", "GET, PUT, POST, OPTIONS, DELETE")
 		if r.Method == "OPTIONS" {
 			return
 		}
